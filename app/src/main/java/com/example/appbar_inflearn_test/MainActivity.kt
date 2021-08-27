286379f12fe69034413bb4f78b15b631ba527b70
ljws93 <ljws93@naver.com>
2021-08-28 08:50:56 +0900

    How to build Appbar Layout
    1. basic usage of Appbar layout
    - change text options when Appbar is expanded or collasped
    
    2. set imageView in the Appbar layout
    - show when it is expanded

--- app/src/main/java/com/example/appbar_inflearn_test/MainActivity.kt
+++ app/src/main/java/com/example/appbar_inflearn_test/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.appbar_inflearn_test
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import com.example.appbar_inflearn_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binder = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binder.root)
-        
+
         setSupportActionBar(binder.toolbar)
+
+        // 접었을 때 타이틀 색 지정
+        binder.toolbarLayout.setCollapsedTitleTextColor(Color.WHITE)
+        // 펼쳤을 때 타이틀 색 지정
+        binder.toolbarLayout.setExpandedTitleColor(Color.GREEN)
+
+        // 접었을 때 타이틀 위치 지정
+        binder.toolbarLayout.collapsedTitleGravity = Gravity.CENTER_HORIZONTAL
+        // 펼쳤을 때 타이틀 위치 지정
+        binder.toolbarLayout.expandedTitleGravity = Gravity.LEFT + Gravity.BOTTOM
+
+        // 이미지뷰 넣기
+        binder.imageView.setImageResource(R.drawable.ic_launcher_foreground)
+
+        // 타이틀 변경
+        supportActionBar?.title = "타이틀입니다"
     }
 }
