bf9a8cd5dde845b5d182d18f71633f19b6d6e10a
ljws93 <ljws93@naver.com>
2021-08-28 09:16:18 +0900

    set switch function turn on/off Appbar expand

--- app/src/main/java/com/example/appbar_inflearn_test/MainActivity.kt
+++ app/src/main/java/com/example/appbar_inflearn_test/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.appbar_inflearn_test
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.example.appbar_inflearn_test.databinding.ActivityMainBinding
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binder : ActivityMainBinding
@@ -30,5 +36,27 @@ class MainActivity : AppCompatActivity() {
 
         // 타이틀 변경
         supportActionBar?.title = "타이틀입니다"
+
+        // 슬라이드를 하더라도 앱바가 그대로 있게 변경한다
+        // (이 기능은 켜고 끌 수 있다) = true: 켠다 / false: 끈다
+        ((binder.appbar.layoutParams as CoordinatorLayout.LayoutParams)
+            .behavior as BlockableAppBarLayoutBehavior).isShouldScroll = false
+    }
+
+    class BlockableAppBarLayoutBehavior(context: Context, attrs: AttributeSet) : AppBarLayout.Behavior(context, attrs) {
+        var isShouldScroll = false
+
+        override fun onStartNestedScroll(
+            parent: CoordinatorLayout,
+            child: AppBarLayout,
+            directTargetChild: View,
+            target: View,
+            nestedScrollAxes: Int,
+            type: Int) = isShouldScroll
+
+        override fun onTouchEvent(
+            parent: CoordinatorLayout,
+            child: AppBarLayout,
+            ev: MotionEvent) = isShouldScroll && super.onTouchEvent(parent, child, ev)
     }
 }
